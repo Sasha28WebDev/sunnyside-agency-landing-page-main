b5e00b444c83f4a778c452cd55680ee93c8ad3f2
Sasha28WebDev <Sasha28WebDev@gmail.com>
2022-01-17 22:16:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var x = document.getElementById("contain");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav");
+  element.classList.toggle("hidden");
+}
